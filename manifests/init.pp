ed838ed356d3c0698182f916bbd506f14cc45f80
root <root@sniperlok4.mylabserver.com>
2017-05-11 06:57:12 +0000

    ntp fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 class ntp (
-   String $package_name = $ntp::params::package_name,
-   String $package_ensure = $ntp::params::package_ensure,
-   String $config_name = $ntp::params::config_name,
-   String $config_file_mode = $ntp::params::config_file_mode,
-   Array[String] $servers = $ntp::params::servers,
-   String $service_ensure = $ntp::params::service_ensure,
-   Boolean $service_enable = $ntp::params::service_enable,
-   Boolean $service_hasrestart  = $ntp::params::service_hasstatus,
-   Boolean $service_hasstatus = $ntp::params::service_hasstatus,
+   String $package_name        = $ntp::params::package_name,
+   String $package_ensure      = $ntp::params::package_ensure,
+   String $config_name         = $ntp::params::config_name,
+   String $config_file_mode    = $ntp::params::config_file_mode,
+   Array[String] $servers      = $ntp::params::servers,
+   String $service_ensure      = $ntp::params::service_ensure,
+   Boolean $service_enable     = $ntp::params::service_enable,
+   Boolean $service_hasrestart = $ntp::params::service_hasrestart,
+   Boolean $service_hasstatus  = $ntp::params::service_hasstatus,
   
  )inherits ntp::params {
 
